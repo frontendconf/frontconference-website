dcf3d57a3d2e5b31fc514c3b25fdb5766db6ef77
Christian Sany <christian@sany.ch>
2023-04-07 23:16:00 +0200

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+// import react from "@astrojs/react";
+// import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // output: "server",
+  // adapter: vercel(),
+  // integrations: [react()],
+});
