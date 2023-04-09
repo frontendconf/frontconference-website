13582ee9f4546594eedf70f5415c35ebdc1815a1
Christian Sany <christian@sany.ch>
2023-04-09 22:55:19 +0200

    Add Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from "astro/config";
 // import react from "@astrojs/react";
-// import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  // output: "server",
-  // adapter: vercel(),
+  output: "server",
+  adapter: vercel(),
   // integrations: [react()],
+  server: {
+    headers: {
+      // Default cache-control for all pages
+      // https://vercel.com/docs/concepts/functions/serverless-functions/edge-caching#recommended-cache-control
+      "Cache-Control": "s-maxage=300, stale-while-revalidate",
+    },
+  },
 });
