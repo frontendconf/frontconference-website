cb994e8045d6d3ce1299fd57f2293ebaa5dfd9d3
Christian Sany <christian@sany.ch>
2024-03-22 20:56:00 +0100

    pls work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from "astro/config";
-// import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  experimental: {
+    assets: true,
+  },
+  adapter: vercel({
+    imageService: true,
+  }),
   // integrations: [react()],
   // server: {
   //   headers: {
