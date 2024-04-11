5beb5db0c2b41f8558eb99c988557de93fcfeca3
Christian Sany <christian@sany.ch>
2024-04-12 00:14:34 +0200

    Finalize Header

--- src/utils/graphql-client.ts
+++ src/utils/graphql-client.ts
@@ -1,5 +1,6 @@
 import { GraphQLClient } from "graphql-request";
 
+// TODO: Setup for Prod environment with env variables and everything
 export const client = new GraphQLClient(
   "https://graphql.contentful.com/content/v1/spaces/j8md9ikrag2j/environments/test?access_token=c0eae4b4c6ddb1acdef7590927e9c901eca36f51fffd93a540a13a756d3278a4"
 );
