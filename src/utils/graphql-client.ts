09b96a957d464512de0d896d16895a682ef81f6c
Manuel Lieb <manuel.lieb@unic.com>
2024-05-31 14:48:40 +0200

    feat: change graphql client credentials

--- src/utils/graphql-client.ts
+++ src/utils/graphql-client.ts
@@ -1,6 +1,9 @@
 import { GraphQLClient } from "graphql-request";
 
-// TODO: Setup for Prod environment with env variables and everything
+const spaceId = import.meta.env.CONTENTFUL_SPACE_ID;
+const environment = import.meta.env.CONTENTFUL_ENVIRONMENT;
+const token = import.meta.env.CONTENTFUL_ACCESS_TOKEN;
+
 export const client = new GraphQLClient(
-  "https://graphql.contentful.com/content/v1/spaces/j8md9ikrag2j/environments/test?access_token=c0eae4b4c6ddb1acdef7590927e9c901eca36f51fffd93a540a13a756d3278a4"
+	`https://graphql.contentful.com/content/v1/spaces/${spaceId}/environments/${environment}?access_token=${token}`,
 );
