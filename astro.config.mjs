7335f4adabc0fe855d9a69298ba0526169bf37de
Thomas Jaggi <thomas@responsive.ch>
2025-09-26 11:35:54 +0200

    feat: update astro and node adapter config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
+  output: "static",
   adapter: node({
     mode: 'standalone',
   }),
