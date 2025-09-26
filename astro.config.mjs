c3ac273994bdbeb521c2725c3b00c875328b702f
Thomas Jaggi <thomas@responsive.ch>
2025-09-26 11:20:48 +0200

    feat: explore deploio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  adapter: vercel(),
   integrations: [icon({
     iconDir: "src/assets/icons",
     svgoOptions: {
