14d3be1ede987a025ef6a76bd27efddfca7cab31
Thomas Jaggi <thomas@responsive.ch>
2025-09-26 11:24:22 +0200

    feat: add node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
+  adapter: node({
+    mode: 'standalone',
+  }),
   integrations: [icon({
     iconDir: "src/assets/icons",
     svgoOptions: {
